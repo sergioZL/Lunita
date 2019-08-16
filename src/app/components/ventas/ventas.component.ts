12d38ee5cfe4cee0f336d987e9d47e2d5117e7dc
root <serch951zl@gmail.com>
2019-08-15 22:02:41 -0500

    se agregaron comentarios al codigo

--- src/app/components/ventas/ventas.component.ts
+++ src/app/components/ventas/ventas.component.ts
@@ -1,3 +1,10 @@
+/**
+ * autor:  Sergio Zermeño López 
+ * fecha:   5/8/2019
+ * proyecto: Este es un componente del proyecto Lunita (proyecto final de programación de quinto 
+ *           cuatrimestre)
+ * 
+ */
 import { Component, OnInit } from '@angular/core';
 import { ArticulosService } from '../../services/articulos-service.service';
 import { LocalStorageService } from '../../services/local-storage.service';
@@ -10,7 +17,6 @@ import { DialogService } from 'src/app/services/dialog.service';
 })
 
 export class VentasComponent implements OnInit {
-  buscador:boolean = true;
   //Contiene las opciones de busqueda para buscar productos en el inventario
   opcionesBusqueda=[
     {
@@ -35,11 +41,11 @@ export class VentasComponent implements OnInit {
   seleccion: string = '';//Almacena la opcion seleccionada en el segundo mat option
   tarticulos=null; //Almacena los articulos recibidos del servidor
   articulos:Array<Object> = [];//Almacena los articulos consultados al servidor por medio del metodo buscar
-  articuloBuscado = null;
+  articuloBuscado = null;//Almacena el articulo obtenido como resultado de la busqueda
   total = 0;
   importe = 0;
   iva = 0;
-  art={//Guarda la información de un articulo seleccionado
+  art={//Guarda la información de un articulo seleccionado para que este sea mostrado en el formulario
     codigo:null,
     descripcion:null,
     costo:null,
@@ -52,30 +58,25 @@ export class VentasComponent implements OnInit {
     cantidad:null,
     existencias:null
   }
-  radioSelect:boolean = true;
-  radioButons = [
-    true,
-    false
-  ];
-  constructor( private articulosServicio: ArticulosService,
-               private localStorageService:LocalStorageService,
-               private dialogService:DialogService ) { }
+  constructor( private articulosServicio: ArticulosService,//se instancia el servicio de articulos
+               private localStorageService:LocalStorageService,//se instancia el servicio para utilizar el localstorage
+               private dialogService:DialogService ) { }//se instancia el servicio para mostrar ventanas popup en angular
 
   ngOnInit() {
-    this.recuperarTodos();
+    this.recuperarTodos();//Este metodo recupera todos los productos contenidos en la base de datos
   }
-  hayRegistros() {
+  hayRegistros() {//esta funcion sirve para consultar si hay articulos en la lista de ventas
     if(this.articulos!=null)return true;
     else return false;
   }
-  limpiarLista(){
+  limpiarLista(){//Elimina los elementos contenidos en la lista articulos
     this.articulos = [];
     this.importe = null;
     this.iva = null;
     this.total = null;
   }
 
-  Vender(){
+  Vender(){            //Envia los datos de la venta al servidor por medio de la función vender del servicio articulos
     if( this.articulos != null && this.articulos.length > 0){
       let venta={
         usuario:this.localStorageService.obtener_localstorage(),
@@ -86,7 +87,7 @@ export class VentasComponent implements OnInit {
       this.articulosServicio.vender(venta).subscribe(result=>{
         console.log(result);
         sale = result;
-        this.dialogService.openConfirmDialog(sale,this.importe,this.iva,this.total)
+        this.dialogService.openConfirmDialog(sale,this.importe,this.iva,this.total)//sirve para generar el ticket de compra
         .afterClosed().subscribe(result=>{
 
           this.limpiarLista();
@@ -98,30 +99,16 @@ export class VentasComponent implements OnInit {
     }else alert('¡Primero debes de agregar productos a la lista de venta!');
   }
 
-  seleccionar(arti,index){
+  seleccionar(arti,index){//sirve para que el producto seleccionado de la lista se cargue en el formulario
     this.art = arti;
     this.borrarDeLista(index);
   }
-  borrarDeLista(indice){
+  borrarDeLista(indice){//sirve para eliminar un producto de la lista
     let arti =this.articulos.splice(indice,1);
     console.log(arti);
     
   }
-  cargarPorNombre(){
-    if(this.art.descripcion != null){
-      let criterios={
-        criterio:'nombre',
-        buscar:this.art.descripcion
-      }
-      this.articulosServicio.buscarPor(criterios).subscribe(result=>{
-        if(result){
-          this.art = result[0]
-        }
-        else alert('No existe ningun articulo con ese nombre')
-      })
-    }
-  }
-  Agregar(){
+  Agregar(){//agrega el articulo mostrado en el formulario a la tabla de articulos que seran vendidos
     if( this.art.cantidad < this.art.existencias  ){
 
       if( this.art.cantidad  < 10 )  this.art.presio = this.art.menudeo;
@@ -149,7 +136,8 @@ export class VentasComponent implements OnInit {
       this.Buscar();
     }else alert('Por el momento no disponemos de esa cantidad de '+this.art.descripcion+' en el inventario');
   }
-  cargarNombres(){
+
+  cargarNombres(){//carga los nombres de los articulos al mat option descripciones
     let i =0;
     let sw;
     let descripciones: Array<Object> = [];
@@ -173,7 +161,8 @@ export class VentasComponent implements OnInit {
     }
     this.opcionesDescripciones[0].opciones = descripciones;    
   }
-  Buscar(){
+
+  Buscar(){//Busca los productos que correspondan a los criterios de busqueda seleccionados en los matoption
     let criterios={
       criterio:this.seleccionada,
       buscar:this.seleccion
@@ -183,13 +172,15 @@ export class VentasComponent implements OnInit {
       this.cargarNombres();
     })
   }
-  recuperarTodos() {
+
+  recuperarTodos() {//Recupera todos los productos de la base de datos
     this.articulosServicio.recuperarTodos().subscribe(result =>{ 
       this.tarticulos = result;
       this.cargarOpciones();
     });
   }
-  cargarOpciones(){
+
+  cargarOpciones(){//Carga las opciones que seran mostradas en los mat option
     let i =0;
     let departamentos: Array<Object>  = [];
     let tipos:Array<Object>  = [];
