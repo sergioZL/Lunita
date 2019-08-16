495ad2a8a30c772475dcdccf66d871ce2fb1084a
root <serch951zl@gmail.com>
2019-08-15 20:49:27 -0500

    m

--- src/app/components/map-component/map-component.component.ts
+++ src/app/components/map-component/map-component.component.ts
@@ -6,9 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-component.component.css']
 })
 export class MapComponentComponent implements OnInit {
-  title: string = 'Rutas';
-  lat: number = 21.839156;
-  lng: number =-102.353242;
+  clientes:Array<Object> = [];
+  title: string = 'clientes';
+  lat: number = 21.928142;
+  lng: number = -102.284551;
+
+  lat1: number = 21.927226;
+  lng1: number = -102.280952;
+
+  lat2: number = 21.928142;
+  lng2: number = -102.284551;
+
+  lat3: number = 21.930449;
+  lng3: number = -102.280677;
+
+  lat4: number = 21.927597;
+  lng4: number = -102.277314;
+
+  lat5: number = 21.932366;
+  lng5: number =  -102.280291;
+
+  lat6: number = 21.931877;
+  lng6: number = -102.282769;
 
   constructor() { }
 
